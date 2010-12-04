ef1d5b56cbdc5158ce300da8684773baf29dc3d8
Michael Gorsuch <michael.gorsuch@gmail.com>
2010-12-04 17:02:18 -0500

    minor documentation for our sample

--- samples/example.rb
+++ samples/example.rb
@@ -10,13 +10,16 @@ Dir.mkdir('/tmp/graphs') if !File.exists?('/tmp/graphs')
 
 LogTrend.start("test.log") do |l|
   
+  # set new locations for our graphs and rrds.  defaults to '.'
   l.rrd_dir = '/tmp/rrd'
   l.graphs_dir = '/tmp/graphs'
   
+  # add some things to trend.  An RRD is built for each one of these items.
   l.add_trend(:total) {|line| line.match /.*/}
   l.add_trend(:fbod) {|line| line.match /fogbugz.com/}
   l.add_trend(:kod) {|line| line.match /kilnhg.com/}
   
+  # build a graph showing requests per minute
   l.add_graph(:requests_per_minute) do |g|
     g.add_point :area, :total, "#333333"
     g.add_point :line, :fbod, "#0066cc"
